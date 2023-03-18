39cba60e100735e771b9ec9db68ff296d5069169
Abdul Sadiq <sadiq9707336@gmail.com>
2023-03-17 21:26:58 -0400

    create method to map json data into IMovie objects

--- src/app/data/service/movies-api.service.ts
+++ src/app/data/service/movies-api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
+import { Method } from '../schema/method';
+import { IMovie, JSONMovie } from '../schema/movie';
 import moviesData from './json/movies_data.json';
 
 @Injectable({
@@ -8,7 +10,50 @@ import moviesData from './json/movies_data.json';
 export class MoviesApiService {
   constructor() {}
 
-  getAll(): Observable<any> {
-    return of(moviesData);
+  getAll(): Observable<IMovie[]> {
+    return new Observable((observer: Subscriber<IMovie[]>) => {
+      observer.next(
+        moviesData.map((movie: JSONMovie) => this.mapJSONMovieToIMovie(movie))
+      );
+    });
+  }
+
+  mapJSONMovieToIMovie(jsonMovie: JSONMovie): IMovie {
+    return {
+      date: new Date(`${jsonMovie.date}, 2019`),
+      title: jsonMovie.title,
+      mani: jsonMovie.mani === 'Yes',
+      nida: jsonMovie.nida === 'Yes',
+      method: this.setMovieMethod(jsonMovie.method),
+    };
+  }
+
+  setMovieMethod(method: string): Method {
+    switch (method) {
+      case 'Cinema':
+        return Method.Cinema;
+      case 'TV':
+        return Method.TV;
+      case 'Netflix':
+        return Method.Netflix;
+      case 'YouTube':
+        return Method.YouTube;
+      case 'Stream':
+        return Method.Stream;
+      case 'Torrent':
+        return Method.Torrent;
+      case 'DVD/Blu-Ray':
+        return Method['DVD/Blu-Ray'];
+      case 'Amazon':
+        return Method.Amazon;
+      case 'Cineplex Rental':
+        return Method['Cineplex Rental'];
+      case 'Airplane':
+        return Method.Airplane;
+      case 'Tubi':
+        return Method.Tubi;
+      default:
+        return Method.Other;
+    }
   }
 }
