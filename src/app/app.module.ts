393302402880a6d163464684b9a60ac86b6aacec
Abdul Sadiq <sadiq9707336@gmail.com>
2023-03-29 17:49:41 -0400

    install ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, SharedModule],
+  imports: [BrowserModule, HttpClientModule, SharedModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
