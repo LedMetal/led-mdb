0ed7275dafe306f3432cb6a1700e2041fa5abe6a
Abdul Sadiq <sadiq9707336@gmail.com>
2023-04-02 19:01:26 -0400

    create and implement table theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, SharedModule, NgbModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    NgbModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
